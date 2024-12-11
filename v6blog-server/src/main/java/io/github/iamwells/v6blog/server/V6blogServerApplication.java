ce7fc665454918bc79db5dde899dca2a1f838f51
iamwells <iamwells@163.com>
2024-12-11 17:28:03 +0800

    feat(v6blog-server): Initial commit
    初始化提交

---
+++ v6blog-server/src/main/java/io/github/iamwells/v6blog/server/V6blogServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.iamwells.v6blog.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V6blogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V6blogServerApplication.class, args);
+    }
+
+}
---
+++ v6blog-server/src/test/java/io/github/iamwells/v6blog/server/V6blogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.iamwells.v6blog.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V6blogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
