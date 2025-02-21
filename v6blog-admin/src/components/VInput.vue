f5aa126f5067b48aea1a68d25c1ea8090867f497
iamwells <iamwells@163.com>
2025-02-21 17:41:09 +0800

    feat(VInput): 优化输入框组件布局和样式
    - 添加了 div 容器以改善组件结构
    - 引入了标签文本和验证器提示
    - 增加了与用户输入相关的图标
    - 调整了输入框的占位符文本

--- v6blog-admin/src/components/VInput.vue
+++ v6blog-admin/src/components/VInput.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <label class="input validator">
-    <input type="text" placeholder="Type here" class="input" />
-  </label>
+  <div>
+    <label class="input validator">
+      <span class="label">用户名</span>
+      <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></g></svg>
+      <input type="text" placeholder="请输入用户名" required />
+    </label>
+    <p class="validator-hint">1234</p>
+  </div>
 </template>
 
 <style scoped></style>
