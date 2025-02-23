392d6d29c0bff23edf71d4cfcb2d0d9466f9e544
iamwells <iamwells@163.com>
2025-02-24 01:50:04 +0800

    feat(admin): 添加图标支持并优化输入组件
    - 在项目中引入 @iconify/tailwind4 依赖
    - 更新 VInput 组件，增加图标支持和多种属性
    - 在 LoginView 中使用新的 VInput 组件

--- v6blog-admin/src/components/VInput.vue
+++ v6blog-admin/src/components/VInput.vue
@@ -1,9 +1,93 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  class: {
+    type: String,
+  },
+  width: {
+    type: String,
+  },
+  height: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: 'text',
+  },
+  value: {
+    type: String,
+  },
+  placeholder: {
+    type: String,
+  },
+  title: {
+    type: String,
+  },
+  readonly: {
+    type: Boolean,
+  },
+  minlength: {
+    type: Number,
+  },
+  maxlength: {
+    type: Number,
+  },
+  min: {
+    type: Number,
+  },
+  max: {
+    type: Number,
+  },
+  icon: {
+    type: String,
+  },
+  iconSize: {
+    type: String,
+  },
+  ifValid: {
+    type: Boolean,
+  },
+  required: {
+    type: Boolean,
+  },
+  pattern: {
+    type: String,
+  },
+  validatorHint: {
+    type: String,
+  },
+})
+
+const iconName = computed(() => {
+  return props.icon ? `icon-[${props.icon.replace(':', '--')}]` : props.icon
+})
+</script>
 
 <template>
-  <label class="input validator">
-    <input type="text" placeholder="Type here" class="input" />
-  </label>
+  <div>
+    <label
+      class="input"
+      :class="[ifValid && 'validator', props.class]"
+      :style="{ width: width, height: height }"
+    >
+      <span v-if="icon" :class="iconName" :style="{ width: iconSize, height: iconSize }"></span>
+      <input
+        :type="type"
+        :value="value"
+        :placeholder="placeholder"
+        :title="title"
+        :required="required"
+        :minlength="minlength"
+        :maxlength="maxlength"
+        :min="min"
+        :max="max"
+        :pattern="pattern"
+        :readonly="readonly"
+      />
+    </label>
+    <p v-if="ifValid && validatorHint" class="validator-hint">{{ validatorHint }}</p>
+  </div>
 </template>
 
 <style scoped></style>
--- v6blog-admin/src/views/auth/LoginView.vue
+++ v6blog-admin/src/views/auth/LoginView.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import VInput from '@/components/VInput.vue'
+import { ref } from 'vue'
+
+const formRef = ref(null)
+const onSubmit = () => {
+  console.log('onSubmit', (formRef.value as unknown as HTMLFormElement))
+}
 </script>
 
 <template>
@@ -15,9 +21,23 @@ import VInput from '@/components/VInput.vue'
         v6blog Admin
         <span class="badge badge-primary">登录</span>
       </h1>
-      <form class="flex flex-col items-center gap-4">
-        <VInput />
+      <form class="flex flex-col items-center gap-4" @submit.prevent="onSubmit" ref="formRef">
+        <VInput
+          type="text"
+          class="input-xl"
+          placeholder="用户名"
+          icon="line-md--account"
+          icon-size="1.2em"
+          :minlength="2"
+          validatorHint="格式不对！"
+          :if-valid="true"
+          width="500px"
+          height="80px"
+        />
+        <button class="btn" type="submit">Submit form</button>
       </form>
+
+      <button class="btn" @click="onSubmit">Submit form</button>
     </div>
   </div>
 </template>
